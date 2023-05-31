2d75138d9588da748083e179b3d5a9f8435e9592
BOOK <marlovobook@hotmail.com>
2023-05-31 19:11:30 +0700

    Fail to transfer data to Postgres

--- mnt/dags/template_pipeline.py
+++ mnt/dags/template_pipeline.py
@@ -10,7 +10,7 @@ import boto3
 from airflow.models import Variable
 
 from airflow import DAG
-from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.amazon.aws.operators.s3 import S3FileTransformOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.providers.amazon.aws.transfers.sql_to_s3 import SqlToS3Operator
@@ -19,11 +19,12 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.hooks.S3_hook import S3Hook
 from airflow.operators.dummy import DummyOperator
 from airflow.providers.amazon.aws.operators.athena import AthenaOperator
+from airflow.providers.mysql.transfers.s3_to_mysql import S3ToMySqlOperator
 
 default_args = {
     'owner' : 'BOOK',
-    'retries': 1,
-    'retry_delay': timedelta(seconds=10),
+    'retries': 2,
+    'retry_delay': timedelta(seconds=15),
     'catchup' : False
 }
 s3_hook = S3Hook(aws_conn_id='minio')
@@ -59,25 +60,9 @@ session_folder = f"src/session/{{{{execution_date.strftime('%Y/%m')}}}}/table_pr
 def transform_product_to_material(ds, next_ds, data_interval_start):
     # Read CSV file using Pandas
     df = pd.read_csv('dags/temp/table_material_demand.csv', index_col=False)
-   
-    
-    # Transform Product to Materials
-    # df['local_arabica'] = df.apply(lambda row: 0 if row['product_name'] == 'expensive' else (20*row['demand'] if row['product_name'] == 'cheap' else 10* row['demand']) , axis=1 )
-    # df['foreign_arabica'] = df.apply(lambda row: 0 if row['product_name'] == 'cheap' else (10*row['demand'] if row['product_name'] in ['medium','expensive'] else 0), axis=1)
-    # df['robusta'] = df.apply(lambda row: 0 if row['product_name'] in ['cheap', 'medium'] else 10*row['demand'], axis=1)
-    # #Agg
-    # df = df.groupby(['date', 'shop_id'], as_index=False).agg({'local_arabica': 'sum', 'foreign_arabica': 'sum', 'robusta': 'sum'})
-    # #melt coffee beans columns into raw_material rows
-    # df = pd.melt(df, id_vars=['date', 'shop_id'], var_name='raw_material', value_name='demand')
-    # #change demand (g) into (kg)
-    # df['demand_kg'] = df['demand'] / 1000
-    # df = df.drop(columns = ['demand'])
-    # # #if sort by date
-    # # df = df.sort_values(['date'], ['shop_id'])
-    # # df = df.reset_index(drop=True)
-    # #df['date'] = df['date'].astype('dbdate')
     # # Perform query on the data 
     df = df.query(f"date >= '{ds}' and date < '{next_ds}'")
+    #df = pd.to_datetime(df['date'])
     # Upload query result back to S3
     query_result_csv = f'dags/result_csv/TEMP_FILE.csv'
     df.to_csv(query_result_csv, index=False)
@@ -90,28 +75,50 @@ def transform_product_to_material(ds, next_ds, data_interval_start):
         replace=True
           )
 
+def _download_file_from_datalake(ds, data_interval_start):
+
+    # Download File from S3
+    ds_str = data_interval_start.strftime('%Y/%m')
+    s3_hook = S3Hook(aws_conn_id="minio")
+    file_name_material = s3_hook.download_file(
+        key=f"src/session/{ds_str}/table_material_demand_{ds}.csv",
+        bucket_name="datalake",
+    )
+
+    return file_name_material
+
+def  _load_data_into_data_warehouse(**context):
+    
+    postgres_hook = PostgresHook(postgres_conn_id="pg_container")
+    conn = postgres_hook.get_conn()
+    cursor = conn.cursor()
+
+    # Get file name from Xcoms
+    file_name_material = context["ti"].xcom_pull(task_ids="download_transformed_file_from_datalake", key="return_value")
+
+    # Copy file to datawarehouse in this case is postgres
+    postgres_hook.copy_expert(
+
+        """
+            COPY
+                dbo.table_material_demand
+
+            FROM STDIN DELIMITER ',' CSV
+    
+        """,
+        file_name_material,
+
+    )
 
 # Define your DAG
 with DAG(
-    dag_id='pipeline_full_v1',
+    dag_id='pipeline_full_v2',
     description='Copy file from PostgreSQL to MinIO, transform data in S3, and upload back to PostgreSQL',
     schedule_interval='@daily',  # Set your desired schedule interval
     start_date=datetime(2023, 5, 20),  # Set the start date of the DAG
 )as dags:
     
     start = DummyOperator(task_id="start")
-
-    # fetch_from_database = SqlToS3Operator(
-    #     task_id="fetch_from_database",
-    #     sql_conn_id='pg_container',
-    #     query='SELECT * FROM dbo.table_product_demand',
-    #     aws_conn_id="minio",
-    #     s3_bucket='datalake',
-    #     s3_key=f"src/table_product_demand.csv",
-    #     replace=True,
-    #     file_format="csv",
-    #     pd_kwargs={"index": False}
-    # )
    
     task_download_from_s3 = PythonOperator(
         task_id='download_from_s3',
@@ -127,9 +134,34 @@ with DAG(
     )
 
     task_upload_to_s3 = PythonOperator(
-        task_id='upload_to_s3',
+        task_id='upload_transfromed_files_to_s3',
         python_callable=transform_product_to_material,
     )
+
+    download_transformed_file_from_datalake = PythonOperator(
+        task_id="download_transformed_file_from_datalake",
+        python_callable=_download_file_from_datalake
+    )
+
+    create_table_in_data_warehouse = PostgresOperator(
+        task_id='create_table_in_data_warehouse',
+        postgres_conn_id="pg_container",
+        sql="""
+            CREATE TABLE IF NOT EXISTS dbo.table_material_demand (
+                date DATE,
+                shop_id VARCHAR(100),
+                raw_material VARCHAR(100),
+                demand_kg VARCHAR(100)
+            )
+        
+        """,
+        )
+    
+    load_data_into_data_warehouse  = PythonOperator(
+        task_id="load_data_into_data_warehouse",
+        python_callable=_load_data_into_data_warehouse
+    )
+
 
     end = DummyOperator(task_id='end')
 
@@ -137,6 +169,6 @@ with DAG(
     #fetch_from_database file to big temp stop
 
     ##download from datalake -> local for transforming -> postgres (as a datawarehouse)
-    start  >> task_download_from_s3 >> task_rename_file >> task_upload_to_s3 >> download_transformed_file end
+    start  >> task_download_from_s3 >> task_rename_file >> task_upload_to_s3 >> download_transformed_file_from_datalake
 
-    download_transformed_file >> create_table_in_data_warehouse >> load_data_into_data_warehouse
+    download_transformed_file_from_datalake >> create_table_in_data_warehouse >> load_data_into_data_warehouse >> end
